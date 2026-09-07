bc18797da47fc5932344032c6670a01bd5a3c167
EnonTaira <taira.e2007@gmail.com>
2026-09-07 10:31:09 +0900

    lnitial commit

---
+++ LA_original_Passing/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  LA_original_Passing
+//
+//  Created by Taira Enon on 2026/09/07.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ LA_original_Passing/LA_original_PassingApp.swift
@@ -0,0 +1,17 @@
+//
+//  LA_original_PassingApp.swift
+//  LA_original_Passing
+//
+//  Created by Taira Enon on 2026/09/07.
+//
+
+import SwiftUI
+
+@main
+struct LA_original_PassingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
